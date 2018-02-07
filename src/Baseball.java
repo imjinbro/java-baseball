615bf98f99f0f68335216a0b5ccd11e3b426e2bf
imjinbro <jinbro@likelion.org>
2018-02-07 18:56:43 +0900

    코드 수정 : 길이 설정

--- src/Baseball.java
+++ src/Baseball.java
@@ -1,14 +1,14 @@
 public class Baseball {
     public static void main(String[] args) {
         Baseball.start(3);
-        Baseball.alertFinish();
+        Baseball.alertFinish(3);
     }
 
     private static void start(int length){
         int[] resultSheet = null;
         int[] answer = AnswerCreator.getAnswer(length);
 
-        while(!isFinish(resultSheet)){
+        while(!isFinish(resultSheet, length)){
             int[] userAnswer = UserAnswerGetter.getUserAnswer(length);
             resultSheet = Matcher.getResult(answer, userAnswer);
             String resultMessage = ResultMessageBuilder.build(resultSheet);
@@ -16,13 +16,13 @@ public class Baseball {
         }
     }
 
-    private static void alertFinish(){
-        printMessage("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    private static void alertFinish(int length){
+        printMessage(length +"개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
 
-    private static boolean isFinish(int[] resultSheet){
-        return isExistResult(resultSheet) && resultSheet[0] == 3;
+    private static boolean isFinish(int[] resultSheet, int length){
+        return isExistResult(resultSheet) && resultSheet[0] == length;
     }
 
     private static boolean isExistResult(int[] resultSheet){
